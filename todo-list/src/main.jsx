035cc6046666061526c3fd7bc0e3b3dbed930abc
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-09-19 14:11:36 +0530

    #1 Remove the all project file from this branch

--- todo-list/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MainScreen from './MainScreen'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MainScreen />
-  </React.StrictMode>
-)
